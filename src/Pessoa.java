1e2ac5f596a5dbc85ae710cd730b355f9c5587f2
fnorton <fillipenorton@gmail.com>
2013-03-08 12:25:52 -0300

    Primeiro commit

---
+++ src/Pessoa.java
@@ -0,0 +1,18 @@
+
+public class Pessoa {
+	private String nome;
+	private String endereco;
+	private String cpf;
+	private String naturalidade;
+	private int idade;
+	
+	public Pessoa(String nome, String endereco, String cpf, String naturalidade, int idade){
+		this.nome = nome;
+		this.endereco = endereco;
+		this.cpf = cpf;
+		this.naturalidade = naturalidade;
+		this.idade = idade;
+	}
+
+	
+}
