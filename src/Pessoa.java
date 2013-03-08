8ca4652f244a19c673e66c9d271fa73892fb3934
fnorton <fillipenorton@gmail.com>
2013-03-08 12:29:39 -0300

    Gerado getters e setters da classe Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -14,5 +14,43 @@ public class Pessoa {
 		this.idade = idade;
 	}
 
-	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
